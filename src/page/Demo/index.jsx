982def1685a3f70499113dd2e6f4ed5785096455
bhavinkarad <bhavinkarad@gmail.com>
2018-12-26 11:09:08 +0530

    add chat horizontal draggable

--- src/page/Demo/index.jsx
+++ src/page/Demo/index.jsx
@@ -1,14 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
+import { Rnd } from 'react-rnd';
 
-function Demo() {
-  return (
-    <Fragment>
-      <div className="container text-center">
-        <b>TrollBox Demo</b>
-      </div>
-    </Fragment>
-  );
+
+import TrollBox from '../TrollBox';
+
+class TrollBoxContainer extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="container text-center">
+          <b>TrollBox Demo</b>
+        </div>
+        <div style={{ position: 'absolute', bottom: 0 }}>
+          <Rnd
+            style={{ position: 'relative' }}
+            dragAxis="x"
+            bounds="window"
+            enableUserSelectHack={false}
+          >
+            <TrollBox />
+          </Rnd>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 
-export default Demo;
+export default TrollBoxContainer;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../page/Home';
 import Calculator from '../page/Calculator';
 import Demo from '../page/Demo';
-import TrollBox from '../page/TrollBox';
 
 const AppRouter = () => (
   <Fragment>
-      <Router>
-        <Fragment>
-          <Route path="/" exact component={Home} />
-          <Route path="/register" exact component={Home} />
-          <Route path="/calculator/" component={Calculator} />
-          <Route path="/demo/" component={Demo} />
-        </Fragment>
-      </Router>
-      <TrollBox />
+    <Router>
+      <Fragment>
+        <Route path="/" exact component={Demo} />
+        <Route path="/register" exact component={Home} />
+        <Route path="/calculator/" component={Calculator} />
+        <Route path="/demo/" component={Demo} />
+      </Fragment>
+    </Router>
   </Fragment>
-    );
-    
-    export default AppRouter;
+);
+
+export default AppRouter;
